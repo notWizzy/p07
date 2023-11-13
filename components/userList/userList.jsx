8e2da3da7b02024cb8f686d492655ecd87f63680
Dmool <143208768+dmool@users.noreply.github.com>
2023-11-13 14:58:59 -0500

    test

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-    List,
-    ListItemButton,
-    ListItemText,
+  List,
+  ListItemButton,
+  ListItemText,
 }
-    from '@mui/material';
+from '@mui/material';
 import './userList.css';
 import axios from 'axios';
 
@@ -15,9 +15,9 @@ class UserList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: undefined,
-            user_id: undefined
-        };
+                users: undefined,
+                user_id: undefined
+            };
     }
 
     componentDidMount() {
@@ -29,46 +29,47 @@ class UserList extends React.Component {
         //console.log(new_user_id);
         const current_user_id = this.state.user_id;
         //console.log(current_user_id);
-        if (current_user_id !== new_user_id) {
+        if (current_user_id  !== new_user_id){
             this.handleUserChange(new_user_id);
         }
     }
 
-    handleUserChange(user_id) {
+    handleUserChange(user_id){
         this.setState({
             user_id: user_id
         });
     }
 
-    handleUserListChange() {
+    handleUserListChange(){
         axios.get("/user/list")
-            .then((response) => {
+            .then((response) =>
+            {
                 this.setState({
                     users: response.data
                 });
             });
     }
 
-    render() {
-        return this.state.users ? (
-            <div>
-                <List component="nav">
-                    {
-                        this.state.users.map(user => (
-                            <ListItemButton selected={this.state.user_id === user._id}
-                                            key={user._id}
-                                            divider={true}
-                                            component="a" href={"#/users/" + user._id}>
-                                <ListItemText primary={user.first_name + " " + user.last_name} />
-                            </ListItemButton>
-                        ))
-                    }
-                </List>
-            </div>
-        ) : (
-            <div />
-        );
-    }
+  render() {
+    return this.state.users ?(
+        <div>
+        <List component="nav">
+            {
+                this.state.users.map(user => (
+                <ListItemButton selected={this.state.user_id === user._id}
+                                key={user._id}
+                                divider={true}
+                                component="a" href={"#/users/" + user._id}>
+                    <ListItemText primary={user.first_name + " " + user.last_name} />
+                </ListItemButton>
+            ))
+            }
+        </List>
+        </div>
+    ) : (
+        <div/>
+    );
+  }
 }
 
 export default UserList;
